4b3c3d542eba91bdf8976d6235b7f04c8bba9723
nicole wakio <nicolewakio@gmail.com>
2019-02-18 14:30:36 +0300

    java.js submit function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,6 @@
-
+var submit = function() {
+  var firstSide = document.getElementById("first");
+  var secondSide = document.getElementById("second");
+  var thirdSide = document.getElementById("third");
+  if(firstSide.value === "" || secondSide.value === "" || thirdSide.value === "")
+  
