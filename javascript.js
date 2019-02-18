9d25edecad82fdf8cc826d8593b054ae8eebc53a
nicole wakio <nicolewakio@gmail.com>
2019-02-18 14:29:36 +0300

    cleared java.js

--- javascript.js
+++ javascript.js
@@ -1,15 +1,1 @@
-var triangle=function(firstSide, secondSide, thirdSide){
-  if(aTriangle(firstSide+secondSide >thirdSide)){
-    alert "triangle"
-  }else {
-    alert "this is not a triangle"
-  }
-};
-var aTriangle=function(firstSide,secondSide,thirdSide){
-  if (firstSide===secondSide&secondSide===thirdSide){
-    alert "equilateral";
-  }elseif(secondSide===firstSide || firstSide===thirdSide ||thirdSide===secondSide){
-  alert "isosceles"}
-}else {
-  alert "scalene"
-}
+
